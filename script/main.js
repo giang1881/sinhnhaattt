b7e2bcb4a243e0f2f1ca8148383beb993d678175
giang <dmgiang@gimasys.com>
2025-05-13 16:57:02 +0700

    g

--- script/main.js
+++ script/main.js
@@ -83,131 +83,131 @@ const animationTimeline = () => {
     tl.to(".container", 0.6, {
         visibility: "visible"
     })
-        .from(".one", 0.7, {
-            opacity: 0,
-            y: 10
-        })
-        .from(".two", 0.4, {
-            opacity: 0,
-            y: 10
-        })
-        .to(".one",
-            0.7,
-            {
-                opacity: 0,
-                y: 10
-            },
-            "+=3.5")
-        .to(".two",
-            0.7,
-            {
-                opacity: 0,
-                y: 10
-            },
-            "-=1")
-        .from(".three", 0.7, {
-            opacity: 0,
-            y: 10
-        })
-        .to(".three",
-            0.7,
-            {
-                opacity: 0,
-                y: 10
-            },
-            "+=3")
-        .from(".four", 0.7, {
-            scale: 0.2,
-            opacity: 0,
-        })
-        .from(".fake-btn", 0.3, {
-            scale: 0.2,
-            opacity: 0,
-        })
-        .staggerTo(
-            ".hbd-chatbox span",
-            1.5, {
-            visibility: "visible",
-        },
-            0.05
-        )
-        .to(".fake-btn", 0.1, {
-            backgroundColor: "rgb(127, 206, 248)",
-        },
-            "+=1")
-        .to(
-            ".four",
-            0.5, {
-            scale: 0.2,
-            opacity: 0,
-            y: -150
-        },
-            "+=1")
-        .from(".idea-1", 0.7, ideaTextTrans)
-        .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
-        .from(".idea-2", 0.7, ideaTextTrans)
-        .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
-        .from(".idea-3", 0.7, ideaTextTrans)
-        .to(".idea-3 strong", 0.5, {
-            scale: 1.2,
-            x: 10,
-            backgroundColor: "rgb(21, 161, 237)",
-            color: "#fff",
-        })
-        .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
-        .from(".idea-4", 0.7, ideaTextTrans)
-        .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
-        .from(
-            ".idea-5",
-            0.7, {
-            rotationX: 15,
-            rotationZ: -10,
-            skewY: "-5deg",
-            y: 50,
-            z: 10,
-            opacity: 0,
-        },
-            "+=1.5"
-        )
-        .to(
-            ".idea-5",
-            0.7, {
-            scale: 0.2,
-            opacity: 0,
-        },
-            "+=2"
-        )
-        .staggerFrom(
-            ".idea-6 span",
-            0.8, {
-            scale: 3,
-            opacity: 0,
-            rotation: 15,
-            ease: Expo.easeOut,
-        },
-            0.2
-        )
-        .staggerTo(
-            ".idea-6 span",
-            0.8, {
-            scale: 3,
-            opacity: 0,
-            rotation: -15,
-            ease: Expo.easeOut,
-        },
-            0.2,
-            "+=1.5"
-        )
-        .to(".slideshow-container", 0.5, {
-            display: "block",
-            onComplete: startSlideshow
-        })
-        .to(".slideshow-container", 0.5, {
-            opacity: 0,
-            onComplete: () => {
-                document.querySelector(".slideshow-container").style.display = "none";
-            }
-        }, "+=39")
+        // .from(".one", 0.7, {
+        //     opacity: 0,
+        //     y: 10
+        // })
+        // .from(".two", 0.4, {
+        //     opacity: 0,
+        //     y: 10
+        // })
+        // .to(".one",
+        //     0.7,
+        //     {
+        //         opacity: 0,
+        //         y: 10
+        //     },
+        //     "+=3.5")
+        // .to(".two",
+        //     0.7,
+        //     {
+        //         opacity: 0,
+        //         y: 10
+        //     },
+        //     "-=1")
+        // .from(".three", 0.7, {
+        //     opacity: 0,
+        //     y: 10
+        // })
+        // .to(".three",
+        //     0.7,
+        //     {
+        //         opacity: 0,
+        //         y: 10
+        //     },
+        //     "+=3")
+        // .from(".four", 0.7, {
+        //     scale: 0.2,
+        //     opacity: 0,
+        // })
+        // .from(".fake-btn", 0.3, {
+        //     scale: 0.2,
+        //     opacity: 0,
+        // })
+        // .staggerTo(
+        //     ".hbd-chatbox span",
+        //     1.5, {
+        //     visibility: "visible",
+        // },
+        //     0.05
+        // )
+        // .to(".fake-btn", 0.1, {
+        //     backgroundColor: "rgb(127, 206, 248)",
+        // },
+        //     "+=1")
+        // .to(
+        //     ".four",
+        //     0.5, {
+        //     scale: 0.2,
+        //     opacity: 0,
+        //     y: -150
+        // },
+        //     "+=1")
+        // .from(".idea-1", 0.7, ideaTextTrans)
+        // .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
+        // .from(".idea-2", 0.7, ideaTextTrans)
+        // .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
+        // .from(".idea-3", 0.7, ideaTextTrans)
+        // .to(".idea-3 strong", 0.5, {
+        //     scale: 1.2,
+        //     x: 10,
+        //     backgroundColor: "rgb(21, 161, 237)",
+        //     color: "#fff",
+        // })
+        // .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
+        // .from(".idea-4", 0.7, ideaTextTrans)
+        // .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
+        // .from(
+        //     ".idea-5",
+        //     0.7, {
+        //     rotationX: 15,
+        //     rotationZ: -10,
+        //     skewY: "-5deg",
+        //     y: 50,
+        //     z: 10,
+        //     opacity: 0,
+        // },
+        //     "+=1.5"
+        // )
+        // .to(
+        //     ".idea-5",
+        //     0.7, {
+        //     scale: 0.2,
+        //     opacity: 0,
+        // },
+        //     "+=2"
+        // )
+        // .staggerFrom(
+        //     ".idea-6 span",
+        //     0.8, {
+        //     scale: 3,
+        //     opacity: 0,
+        //     rotation: 15,
+        //     ease: Expo.easeOut,
+        // },
+        //     0.2
+        // )
+        // .staggerTo(
+        //     ".idea-6 span",
+        //     0.8, {
+        //     scale: 3,
+        //     opacity: 0,
+        //     rotation: -15,
+        //     ease: Expo.easeOut,
+        // },
+        //     0.2,
+        //     "+=1.5"
+        // )
+        // .to(".slideshow-container", 0.5, {
+        //     display: "block",
+        //     onComplete: startSlideshow
+        // })
+        // .to(".slideshow-container", 0.5, {
+        //     opacity: 0,
+        //     onComplete: () => {
+        //         document.querySelector(".slideshow-container").style.display = "none";
+        //     }
+        // }, "+=39")
         .staggerFromTo(
             ".baloons img",
             1.5, {
@@ -341,7 +341,8 @@ function startSlideshow() {
 
     setInterval(() => {
         index = (index + 1) % images.length;
-
+        imgEl.style.transition = "opacity 1s ease-in-out";   // Thêm hiệu ứng mượt
+        captionEl.style.transition = "opacity 1s ease-in-out"; // Thêm hiệu ứng mượt
         // Làm mờ ảnh và caption trước khi đổi
         imgEl.style.opacity = 0;
         captionEl.style.opacity = 0;
